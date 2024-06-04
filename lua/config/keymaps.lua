3f921f0006fe5570689d84a1695a632425a1d013
cristianqsanchez <cristianquintero316@gmail.com>
2024-06-04 04:24:24 -0500

    feat: add new keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,16 +1,22 @@
+require('plugins.oil')
+
 vim.g.mapleader = " "
+
+vim.keymap.set("i", "jj", "<Esc>")
 
 vim.keymap.set("n", "<leader>j", "<C-w><C-h>")
 vim.keymap.set("n", "<leader>k", "<C-w><C-l>")
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
 
-vim.keymap.set("n", "<leader><leader>", function()
-	vim.cmd("Telescope")
-end)
+vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
 
-vim.keymap.set("n", "<leader>f", function()
-	vim.cmd("Telescope fd")
-end)
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-vim.keymap.set("n", "<leader>e", function()
-	vim.cmd("Vexplore")
-end)
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- TIP: Disable arrow keys in normal mode
+ vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+ vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+ vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+ vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
