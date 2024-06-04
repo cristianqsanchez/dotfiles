d8ebec1d5bf8335293807bc86146d75b45bfd277
cristianqsanchez <cristianquintero316@gmail.com>
2024-06-03 22:36:34 -0500

    feat: add statusbar and dashboard ui

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,54 @@
+return {
+	{
+		'nvim-lualine/lualine.nvim',
+		event = 'VeryLazy',
+		opts = {
+			options = {
+				theme = 'kanagawa',
+				disabled_filetypes = {
+					'dashboard'
+				}
+			}
+		}
+	},
+
+	{
+		'nvimdev/dashboard-nvim',
+		lazy = false,
+		event = 'VimEnter',
+		config = function()
+			local logo = [[
+		 ██████╗ ███████╗    ██████╗ ███████╗██╗   ██╗
+		██╔═══██╗██╔════╝    ██╔══██╗██╔════╝██║   ██║
+		██║   ██║███████╗    ██║  ██║█████╗  ██║   ██║
+		██║▄▄ ██║╚════██║    ██║  ██║██╔══╝  ╚██╗ ██╔╝
+		╚██████╔╝███████║    ██████╔╝███████╗ ╚████╔╝ 
+		 ╚══▀▀═╝ ╚══════╝    ╚═════╝ ╚══════╝  ╚═══╝  
+			]]
+
+			logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+			require('dashboard').setup {
+				theme = 'doom',
+				hide = {
+					statusline = true
+				},
+				config = {
+					header = vim.split(logo, "\n"),
+					center = {
+						{ action = "ene | startinsert", desc = 'New file', icon = ' ', key = 'n' },
+						{ action = "Telescope live_grep", desc = 'Search', icon = ' ', key = 's' },
+						{ action = "Telescope fd", desc = 'Search files', icon = ' ',  key = 'f' },
+						{ action = "Telescope oldfiles", desc = 'Recent files', icon = ' ', key = 'r' },
+						{ action = "Lazy", desc = 'Lazy', icon = '󰒲 ', key = 'l' },
+						{ action = "qa", desc = 'Exit', icon = ' ', key = 'q' }
+					},
+					disable_move = true
+				}
+			}
+		end,
+		dependencies = {
+			'nvim-tree/nvim-web-devicons'
+		}
+	}
+}
