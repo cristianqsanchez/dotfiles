622d046ca31d6e32f44aaead273f59f242f4ef40
cristianqsanchez <cristianquintero316@gmail.com>
2024-06-10 23:41:43 -0500

    feat: override colorscheme for special windows

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,34 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
+		require('kanagawa').setup({
+			overrides = function(colors) local theme = colors.theme
+				return {
+					Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+					PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+					PmenuSbar = { bg = theme.ui.bg_m1 },
+					PmenuThumb = { bg = theme.ui.bg_p2 },
+
+					TelescopeTitle = { fg = theme.ui.special, bold = true },
+					TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+					TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+					TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+					TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+					TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+					TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+
+					NormalFloat = { bg = theme.ui.bg_m3 },
+					FloatBorder = { bg = "NONE" },
+					FloatTitle = { bg = "NONE" },
+					NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+					WhichKeyFloat = { bg = theme.ui.bg_m3 },
+
+					LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+					MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+				}
+			end,
+		})
 		vim.cmd([[colorscheme kanagawa-dragon]])
 
 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
