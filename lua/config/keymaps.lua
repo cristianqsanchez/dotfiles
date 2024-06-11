d8f2cc532c2d14ba246c580ddae989debce592e9
cristianqsanchez <cristianquintero316@gmail.com>
2024-06-10 23:37:56 -0500

    feat: add new amazing remaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,21 +2,28 @@ require('plugins.oil')
 
 vim.g.mapleader = " "
 
-vim.keymap.set("i", "jj", "<Esc>")
-
-vim.keymap.set("n", "<leader>j", "<C-w><C-h>")
-vim.keymap.set("n", "<leader>k", "<C-w><C-l>")
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
-
-vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
-
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+vim.keymap.set("i", "jj", "<Esc>")
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
--- TIP: Disable arrow keys in normal mode
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
  vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
  vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
  vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
  vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set('n', '<leader>ca', '<cmd>%bd|e#<CR>', { desc = 'Close all buffers' })
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '%', '%zz')
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace coincidences" })
