fdeba5878c1752006b2e913752f8f9ce63c7c229
cristianqsanchez <cristianquintero316@gmail.com>
2024-06-18 23:50:53 -0500

    feat: improve colorscheme window backgrounds

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -20,21 +20,24 @@ return {
 					TelescopePreviewNormal = { bg = theme.ui.bg_dim },
 					TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
 
-					NormalFloat = { bg = theme.ui.bg_m3 },
-					FloatBorder = { bg = "NONE" },
-					FloatTitle = { bg = "NONE" },
+					NormalFloat = { bg = theme.ui.bg_p1 },
+					FloatBorder = { bg = theme.ui.bg_p1 },
+					FloatTitle = { bg = theme.ui.bg_p1 },
 					NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
 
 					WhichKeyFloat = { bg = theme.ui.bg_m3 },
 
 					LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
 					MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
 				}
 			end,
 		})
 		vim.cmd([[colorscheme kanagawa-dragon]])
 
 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		vim.api.nvim_set_hl(0, "LineNr", { bg = "#282727",  fg = "#FF9E3B", bold = true })
+		vim.api.nvim_set_hl(0, "LineNrAbove", { bg = "#282727", fg = "#625e5a"  })
+		vim.api.nvim_set_hl(0, "LineNrBelow", { bg = "#282727", fg = "#625e5a"  })
 	end
 }
